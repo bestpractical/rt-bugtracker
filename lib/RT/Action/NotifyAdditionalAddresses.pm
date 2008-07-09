5bf1da632147e3c92b3db8b98bd2df5f6d345b9d
Ruslan Zakirov <ruz@bestpractical.com>
2008-07-09 18:58:14 +0000

    add NotifyAdditionalAddresses action

---
+++ lib/RT/Action/NotifyAdditionalAddresses.pm
@@ -0,0 +1,29 @@
+package RT::Action::NotifyAdditionalAddresses;
+
+use strict;
+use warnings;
+use base qw(RT::Action::Notify);
+
+=head1 NAME
+
+RT::Action::NotifyAdditionalAddresses - sends notifications to additional addresses defined for the distribution
+
+=head1 DESCRIPTION
+
+Either maintainer or user with the AdminQueue right can define additional
+addresses for notifications. This is a subclass of L<RT::Action::Notify> that
+notifies those recipients.
+
+=cut
+
+sub SetRecipients {
+    my $self = shift;
+    my $attr = $self->TicketObj->QueueObj->FirstAttribute('NotifyAddresses');
+    return unless $attr;
+    my $addresses = $attr->Content;
+    return unless $addresses && @$addresses;
+
+    $self->{'To'} = [ @$addresses ];
+}
+
+1;
